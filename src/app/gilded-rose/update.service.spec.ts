2de45cf625aa83d539cf37c682a42638bde9e76c
Manuel Joaquin Saavedra Severiche <mjsaavedra@santacruzfg.com>
2021-03-31 18:38:58 -0400

    unit test TDD for sulfur

--- src/app/gilded-rose/update.service.spec.ts
+++ src/app/gilded-rose/update.service.spec.ts
@@ -9,11 +9,11 @@ describe('UpdateService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({})
     service = TestBed.inject(UpdateService)
-  })
+  });
 
   it('should be created', () => {
     expect(service).toBeTruthy()
-  })
+  });
 
   describe('Normal items', () => {
     it('when Q:10, expected Q:9', () => {
@@ -21,34 +21,34 @@ describe('UpdateService', () => {
         type: 'normal',
         quality: 10,
         sellIn: 5,
-      }
+      };
       const result = service.update(normalItem)
       expect(9).toEqual(result.quality)
       expect(4).toEqual(result.sellIn)
-    })
+    });
 
     it('when Q: 5, Q: 4', () => {
       const normalItem: Item = {
         type: 'normal',
         quality: 5,
         sellIn: 2,
-      }
-      const result = service.update(normalItem)
-      expect(4).toEqual(result.quality)
-      expect(1).toEqual(result.sellIn)
-    })
+      };
+      const result = service.update(normalItem);
+      expect(4).toEqual(result.quality);
+      expect(1).toEqual(result.sellIn);
+    });
 
     it('when Q: 0, Q: 0', () => {
       const normalItem: Item = {
         type: 'normal',
         quality: 0,
         sellIn: 0,
-      }
-      const result = service.update(normalItem)
-      expect(0).toEqual(result.quality)
-      expect(0).toEqual(result.sellIn)
-    })
-  })
+      };
+      const result = service.update(normalItem);
+      expect(0).toEqual(result.quality);
+      expect(0).toEqual(result.sellIn);
+    });
+  });
 
   describe('Aged Brie', () => {
     it('when Q: 10, Q: 11', () => {
@@ -56,10 +56,34 @@ describe('UpdateService', () => {
         type: 'agedBrie',
         quality: 10,
         sellIn: 5,
-      }
-      const result = service.update(normalItem)
-      expect(11).toEqual(result.quality)
-      expect(4).toEqual(result.sellIn)
-    })
-  })
-})
+      };
+      const result = service.update(normalItem);
+      expect(11).toEqual(result.quality);
+      expect(4).toEqual(result.sellIn);
+    });
+  });
+
+  describe('Sulfururas', () => {
+    it('when Q: 10, expected Q: 10', () => {
+      const sulfurItem: Item = {
+        type: 'sulfuras',
+        quality: 10,
+        sellIn: 5,
+      };
+      const result = service.update(sulfurItem);
+      expect(10).toEqual(result.quality);
+      expect(5).toEqual(result.sellIn);
+    });
+
+    it('when Q: 50, expected Q: 50', () => {
+      const sulfurItem: Item = {
+        type: 'sulfuras',
+        quality: 50,
+        sellIn: 15,
+      };
+      const result = service.update(sulfurItem);
+      expect(50).toEqual(result.quality);
+      expect(15).toEqual(result.sellIn);
+    });
+  });
+});
